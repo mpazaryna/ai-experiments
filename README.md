379d71567cff76f7b2d0d7152106d855ff9aabe9
mpazaryna <mpazbot@gmail.com>
2024-03-22 11:25:58 -0400

    chore: updates

---
+++ README.md
@@ -0,0 +1,17 @@
+# Streamlit examples
+
+## Create an environment
+
+```sh
+conda create -n [environment] python=3.10
+conda activate [environment]
+```
+
+## Projects
+
+[30 Days](https://30days.streamlit.app)
+
+```sh
+streamlit run thirty-days/day_02.py
+streamlit run thirty-days/day_05.py
+```
--- langchain/introduction/agent_call.py
+++ langchain/introduction/agent_call.py
@@ -1,7 +1,6 @@
 from dotenv import find_dotenv, load_dotenv
 from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.agents.load_tools import get_all_tool_names
-from langchain.prompts import PromptTemplate
 from langchain_openai import OpenAI
 
 # --------------------------------------------------------------
@@ -15,19 +14,19 @@ load_dotenv(find_dotenv())
 # --------------------------------------------------------------
 
 
-def run_agent_demo():
+def agent_demo():
     llm = OpenAI()
     get_all_tool_names()
-    tools = load_tools(["wikipedia", "llm-math"], llm=llm)
+    tools = load_tools(["wikipedia"], llm=llm)
 
     agent = initialize_agent(
         tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
     )
     result = agent.invoke(
-        "In what year was python released and who is the original creator? Multiply the year by 3"
+        "In what year was python programming language released and who is the original creator?"
     )
     return result
 
 
 if __name__ == "__main__":
-    print(run_agent_demo())
+    print(agent_demo())
